5d4192b5cc3019d23512c6e639848dfa3b051f22
letasbulhas <let.bulhoes@gmail.com>
2023-09-12 20:04:53 -0300

    Adicionando as features da API

---
+++ upload-arquvios-api-java/src/main/java/next/cesar/school/uploadarquviosapijava/UploadArquviosApiJavaApplication.java
@@ -0,0 +1,13 @@
+package next.cesar.school.uploadarquviosapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadArquviosApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadArquviosApiJavaApplication.class, args);
+	}
+
+}
---
+++ upload-arquvios-api-java/src/test/java/next/cesar/school/uploadarquviosapijava/UploadArquviosApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package next.cesar.school.uploadarquviosapijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadArquviosApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
